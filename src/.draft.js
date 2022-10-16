9d3bfffaa47941516ab43df34a6eefb2492d799a
Adwasar <user@Users-MacBook-Pro.local>
2022-10-16 23:08:06 +0300

    feat: thomethink that don't work

--- src/.draft.js
+++ src/.draft.js
@@ -17,7 +17,7 @@ class LinkedList {
     return this.length;
   }
   
-  add(value) {
+  addToTail(value) {
     if (this.length === 0) {
       this.head = new Node(value);
     } else {
@@ -27,15 +27,77 @@ class LinkedList {
         current = current.next;
       }
       current.next = new Node(value);
+    }
+    this.length++;
+  }
 
+  removeAt(position) {
+    if (position < 0 || position >= this.length) {
+      return;
     }
 
-    this.length++;
+    let current = this.head; 
 
+    if (position === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = current.next;
+    }
+
+    this.length--; 
+    return current.value;
+  }
+
+  remove(element) {
+    this.removeAt(this.indexOf(element));
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0; 
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+
+  print() {
+    let current = this.head;
+    let arr = [];
+    while(current) {
+      console.log(current.value);
+      arr.push(current.value);
+      current = current.next;
+    }
+    
+    return arr;
   }
 }
 
 let a = new LinkedList();
-a.add('a');
+a.addToTail(1);
+a.addToTail(2);
+a.addToTail(3);
+a.addToTail(4);
+a.addToTail(5);
+a.addToTail(5);
 
-console.log(a.head);
+
+console.log(removeKFromList(a, 3));
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -15,18 +15,126 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
+ 
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ let arr = [1, 3, 5, 5, 6];
+ let num = 5;
+
+function removeKFromList(l, k) {
+  l.forEach((item, i) => {
+    if (item == k) {
+      l.splice(i, 1);
+    }
+  });
+
+  return l;
 }
 
-module.exports = {
-  removeKFromList
-};
+console.log(removeKFromList(arr, num));
+
+
+// module.exports = {
+//   removeKFromList
+// };
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.length = 0;
+
+//   }
+
+//   size() {
+//     return this.length;
+//   }
+  
+//   addToTail(value) {
+//     if (this.length === 0) {
+//       this.head = new Node(value);
+//     } else {
+//       let current = this.head; // ссылка на текущий элемент
+      
+//       while(current.next) { // доходим до самого конца 
+//         current = current.next;
+//       }
+//       current.next = new Node(value);
+//     }
+//     this.length++;
+//   }
+
+//   removeAt(position) {
+//     if (position < 0 || position >= this.length) {
+//       return;
+//     }
+
+//     let current = this.head; 
+
+//     if (position === 0) {
+//       this.head = current.next;
+//     } else {
+//       let prev = null;
+//       let index = 0;
+
+//       while (index < position) {
+//         prev = current;
+//         current = current.next;
+//         index++;
+//       }
+
+//       prev.next = current.next;
+//     }
+
+//     this.length--; 
+//     return current.value;
+//   }
+
+//   remove(element) {
+//     this.removeAt(this.indexOf(element));
+//   }
+
+//   indexOf(element) {
+//     let current = this.head;
+//     let index = 0; 
+
+//     while (current) {
+//       if (current.value === element) {
+//         return index;
+//       }
+
+//       current = current.next;
+//       index++;
+//     }
+
+//     return -1;
+//   }
+
+
+//   print() {
+//     let current = this.head;
+//     let arr = [];
+//     while(current) {
+//       console.log(current.value);
+//       arr.push(current.value);
+//       current = current.next;
+//     }
+    
+//     return arr;
+//   }
+// }
+
+// let a = new LinkedList();
+// a.addToTail(1);
+// a.addToTail(2);
+// a.addToTail(3);
+// a.addToTail(4);
+// a.addToTail(5);
+// a.addToTail(5);
+
